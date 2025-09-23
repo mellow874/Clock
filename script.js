ef5d0dfec710982a1836128124322abb5a9ca8d1
mellow874 <felicitymncube49@gmail.com>
2025-09-23 16:05:42 +0200

    Added some javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const today = new Date(); 
+
+const hour = today.getHours() + 2;
+const minutes = today.getMinutes();
+const seconds = today.getSeconds();
+
+console.log(today);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
